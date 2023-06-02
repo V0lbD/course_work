f0e7bc2e73006d1ba499736ba60a54f25536adfa
V0lbD <90722203+V0lbD@users.noreply.github.com>
2023-06-02 09:41:43 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/course_work.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/University/4th semester/course_work/allocator/memory_base_class.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/allocator/memory_base_class.cpp.obj"
+  "D:/University/4th semester/course_work/allocator/memory_holder.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/allocator/memory_holder.cpp.obj"
+  "D:/University/4th semester/course_work/allocator_from_global_heap/memory_from_global_heap.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/allocator_from_global_heap/memory_from_global_heap.cpp.obj"
+  "D:/University/4th semester/course_work/db/data_base.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/db/data_base.cpp.obj"
+  "D:/University/4th semester/course_work/db_user_communication.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/db_user_communication.cpp.obj"
+  "D:/University/4th semester/course_work/db_value/db_value.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/db_value/db_value.cpp.obj"
+  "D:/University/4th semester/course_work/db_value/db_value_builder.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/db_value/db_value_builder.cpp.obj"
+  "D:/University/4th semester/course_work/logger/logger.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/logger/logger.cpp.obj"
+  "D:/University/4th semester/course_work/logger/logger_builder.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/logger/logger_builder.cpp.obj"
+  "D:/University/4th semester/course_work/logger/logger_holder.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/logger/logger_holder.cpp.obj"
+  "D:/University/4th semester/course_work/main.cpp" "D:/University/4th semester/course_work/cmake-build-debug/CMakeFiles/course_work.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/course_work.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/course_work.dir/allocator/memory_base_class.cpp.obj"
+  "CMakeFiles/course_work.dir/allocator/memory_holder.cpp.obj"
+  "CMakeFiles/course_work.dir/allocator_from_global_heap/memory_from_global_heap.cpp.obj"
+  "CMakeFiles/course_work.dir/db/data_base.cpp.obj"
+  "CMakeFiles/course_work.dir/db_user_communication.cpp.obj"
+  "CMakeFiles/course_work.dir/db_value/db_value.cpp.obj"
+  "CMakeFiles/course_work.dir/db_value/db_value_builder.cpp.obj"
+  "CMakeFiles/course_work.dir/logger/logger.cpp.obj"
+  "CMakeFiles/course_work.dir/logger/logger_builder.cpp.obj"
+  "CMakeFiles/course_work.dir/logger/logger_holder.cpp.obj"
+  "CMakeFiles/course_work.dir/main.cpp.obj"
+  "course_work.exe"
+  "course_work.exe.manifest"
+  "course_work.pdb"
+  "libcourse_work.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/course_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
